590899a2408b7c67d6b2e4650a52c8247034799a
TTurchan <TTurchan1@gmail.com>
2024-02-15 21:23:40 -0500

    update3

--- day3.py
+++ day3.py
@@ -7,7 +7,7 @@ def checkSurrounding(i,j,mat):
                         print(i,j,k,l,mat[k][l])
                         return True
     return False
-with open('day3input.txt') as f:
+with open('day3test.txt') as f:
     lines = f.readlines()
     total = 0
     flag = False
@@ -28,4 +28,46 @@ with open('day3input.txt') as f:
                     temp = 0
                 flag = False
 print(total)
+gearRatio = {}
 
+
+def checkSurroundingStr(i,jmin,jmax,mat,val):
+    for k in range(i-1,i+2):
+        for l in range(jmin-1,jmax+2):
+            if k >=0 and k <len(lines):
+                if l >= 0 and l<len(lines[0])-1:
+                    if mat[k][l] == "*":
+                        if (k,l) in gearRatio:
+                            gearRatio[(k,l)] += [val]
+                        else:
+                            gearRatio[(k,l)] = [val]
+                        return
+
+with open('day3input.txt') as f:
+    lines = f.readlines()
+    total = 0
+    flag = False
+    temp = -1
+    for i in range(len(lines)):
+        temp = 0
+        flag = False
+        initInd = -1 
+        for j in range(len(lines[0])):
+            if lines[i][j] in '0123456789':
+                if initInd == -1:
+                    initInd = j
+                temp = temp*10 + int(lines[i][j])
+            else:
+                if temp !=0:
+                    checkSurroundingStr(i,initInd,j-1,lines,temp)
+                initInd = -1
+                temp = 0
+        if initInd != -1:
+            checkSurroundingStr(i,initInd,len(lines)-1,lines,temp)
+            initInd = -1
+            temp = 0
+totRatio = 0
+for ratio in gearRatio:
+    if len(gearRatio[ratio]) == 2:
+        totRatio += gearRatio[ratio][0] * gearRatio[ratio][1]
+print(totRatio)
